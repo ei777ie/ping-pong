d8f0ec938cc8764afdde62d97ec1a53dab8424bb
ei777ie <129663734+ei777ie@users.noreply.github.com>
2023-04-21 19:22:50 +0300

    v2.0

--- main.py
+++ main.py
@@ -34,28 +34,53 @@ class Player(GameSprite):
             self.rect.y -= 10
 
 ball = GameSprite('ball.png', 200, 200, 60, 60, 60)
-platform1 = Player('platform.png', 0, 150, 60, 125, 125)
-platform2 = Player('platform.png', 575, 150, 60, 125, 125)
+platform1 = Player('platform.png', 0, 150, 60, 75, 150)
+platform2 = Player('platform.png', 624, 150, 60, 75, 150)
 
 
+speed_x = 5
+speed_y = 5
 
+font.init()
+font = font.Font(None, 50)
+
+finish = False
 clock= time.Clock()
 FPS = 60
 game = True
 while game:
-    window.blit(background, (0, 0))
+    if finish != True:
+        window.blit(background, (0, 0))
 
-    ball.reset()
-    ball.update()
+        ball.reset()
 
-    platform1.reset()
-    platform1.update1()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
 
-    platform2.reset()
-    platform2.update2()
+        if ball.rect.y > 500 - 60 or ball.rect.y < 0:
+            speed_y *= -1
 
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    display.update()
-    clock.tick(FPS)
+        if sprite.collide_rect(ball, platform1) or sprite.collide_rect(ball, platform2):
+            speed_x *= -1
+
+        if ball.rect.x <= 1:
+          lose1 = font.render('Левый игрок проиграл!', True, (255, 255, 255))
+          window.blit(lose1, (150, 225))
+          finish = False
+
+        if ball.rect.x >= 699:
+          lose2 = font.render('Правый игрок проиграл!', True, (255, 255, 255))
+          window.blit(lose2, (150, 225))
+          finish = False
+
+        platform1.reset()
+        platform1.update1()
+
+        platform2.reset()
+        platform2.update2()
+
+        for e in event.get():
+            if e.type == QUIT:
+                game = False
+        display.update()
+        clock.tick(FPS)
